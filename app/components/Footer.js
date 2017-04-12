bc453e3e6f8139db84072d2644c6fa8c83256002
Terence Ro <rowyourboat83@gmail.com>
2017-04-12 13:42:25 -0500

    fix paths to images

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -14,16 +14,16 @@ class MainFooter extends React.Component {
             </div>
             <ul>
                 <li>
-                    <a href="https://github.com/terencero" className="btn-social btn-outline"><img src="img/githubv2.png" className="fa fa-fw" width="30" alt="github icon" /></a>
+                    <a href="https://github.com/terencero" className="btn-social btn-outline"><img src="./publicimg/githubv2.png" className="fa fa-fw" width="30" alt="github icon" /></a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/terence-ro/" className="btn-social btn-outline"><img src="img/linkedin_circle_black.png" className="fa fa-fw" width="30" alt="linkedin icon" /></a>
+                    <a href="https://www.linkedin.com/in/terence-ro/" className="btn-social btn-outline"><img src="./public/img/linkedin_circle_black.png" className="fa fa-fw" width="30" alt="linkedin icon" /></a>
                 </li>
                 <li>
-                    <a href="https://twitter.com/tro1109" className="btn-social btn-outline"><img src="img/twitter.png" className="fa fa-fw" width="30" alt="twitter icon" /></a>
+                    <a href="https://twitter.com/tro1109" className="btn-social btn-outline"><img src="./public/img/twitter.png" className="fa fa-fw" width="30" alt="twitter icon" /></a>
                 </li>
                 <li>
-                    <a href="mailto:terencehro@gmail.com" className="btn-social btn-outline"><img src="img/message.png" className="fa fa-fw" width="30" alt="email icon" /></a>
+                    <a href="mailto:terencehro@gmail.com" className="btn-social btn-outline"><img src="./public/img/message.png" className="fa fa-fw" width="30" alt="email icon" /></a>
                 </li>
             </ul>
 
